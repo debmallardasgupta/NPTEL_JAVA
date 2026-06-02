09ce8331c2429a894ba328849d4ba0172eab363b
Debmallar Dasgupta <debmallardasgupta@gmail.com>
2026-06-02 21:55:45 +0530

    initial main structure

---
+++ CryptoComm/src/Main.java
@@ -0,0 +1,12 @@
+package src;
+import javax.crypto.SecretKey;
+import javax.crypto.spec.IvParameterSpec;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+ 
+        Scanner scanner = new Scanner(System.in);
+        
+    }
+}
